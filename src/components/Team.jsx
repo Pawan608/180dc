75e81f33a992811a05f58570cb9b2aa14038eb37
Ankit Kashyap <110721733+SwitchBladeAK@users.noreply.github.com>
2023-12-17 01:20:53 +0530

    Update Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import {profiles} from '../data/team';
-import linkedIn from '../assets/linkedin.png';
-import FadeInSection from '../components/cards/FadeInSection'
+import {profiles} from '/src/data/team';
+import linkedIn from '/src/assets/linkedin.png';
+import FadeInSection from '/src/components/cards/FadeInSection'
 
 
 export default function Team() {
@@ -17,7 +17,7 @@ export default function Team() {
             {profiles.map((profile, idx) => (
               <div className="flex flex-col font-title justify-center items-center p-10">
                 <div className="group flex justify-center items-center h-40 w-40 rounded-full bg-lime-600 border-2 border-lime-300 hover:scale-[1.05] object-center object-cover shadow-2xl">
-                  <img src={profile.imgUrl} alt="..." className="h-40 w-40 group-hover:blur-[2px] rounded-full object-left-top object-cover" />
+                  <img src={profile.imgUrl} alt="/src." className="h-40 w-40 group-hover:blur-[2px] rounded-full object-left-top object-cover" />
                   <div className="absolute invisible group-hover:visible  flex justify-center items-center text-lime-600 font-medium text-xl">
                     <a href={profile.linkedin} target="_blank"><img src={linkedIn} alt="..." className="h-4 w-4 md:h-14 md:w-14 md:mx-2"/></a>
                   </div>
